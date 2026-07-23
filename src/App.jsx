d2f268ec44d4fa6b2b40d423524b58cc6b80aaa2
saleemr78612-tech <saleemr78612@gmail.com>
2026-07-23 20:32:05 +0500

    Fix import path casing for Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./Components/Navbar";
+import Navbar from "./components/Navbar";
 
 import Home from "./pages/Home";
 
